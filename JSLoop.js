65b802544f72ad4d8c8da12b48e26dbc6a305a74
Liam Goodacre <goodacre.liam@gmail.com>
2012-05-13 23:20:38 +0100

    Added variadic version of loop.
    Loop: is variadic.
    Loopwith: is object based.

--- JSLoop.js
+++ JSLoop.js
@@ -8,8 +8,35 @@
  * Author URL : http://liamgoodacre.com
  * GitHub : https://github.com/LiamGoodacre/JSLoop.js
  * Date : 2012_05_13
- */
-var loop = function (data, fn) {
+**/
+var loop;
+(function () {
+  var slice = Array.prototype.slice;
+  loop = function () {
+    var recur, nextdata, result, fn, args, len;
+    args = slice.call(arguments);
+    fn = args.pop();;
+    //  Define recurse
+    recur = function () {
+      //  Store the next iteration data
+      nextdata = slice.call(arguments);
+      //  Pass back the continuation flag
+      return recur;
+    };
+    //  Use the initial data
+    nextdata = args;
+    //  Initial run, should iterate at least once
+    result = recur;
+    //  While the user wishes to recurse
+    while (result == recur) {
+      //  Execute the function, giving it recur and the next iteration data
+      result = fn.apply(undefined, [recur].concat(nextdata));
+    }
+    return result;
+  };
+}());
+
+var loopwith = function (data, fn) {
   var recur, nextdata, result;
   //  Define recurse
   recur = function (newdata) {
